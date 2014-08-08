dd6a672764f3b2c426def19bb2c59c330278b96d
markuss232 <mark.monteiro23@gmail.com>
2014-08-08 17:49:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	
 	$('button').click(function() {
 		startLoader();
-		getlocation(findSmokes);
+		getLocation(findSmokes);
 	});
 	
 	function startLoader() {
@@ -31,15 +31,17 @@ $(document).ready(function() {
 			//   the current GPS coordinates
 			//
 			var onSuccess = function(position) {
+				console.log("Got location: (" + position.coords.latitude + "," + position.coords.longitude + ")");
 				callback(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
 			};
 
 			// onError Callback receives a PositionError object
 			//
 			function onError(error) {
-				alert('Where in the fuck are you?! An error occured while determining your location:\n' +
-					  'code: '    + error.code    + '\n' +
-					  'message: ' + error.message + '\n');
+				console.log("Error determining location:\n" + 
+					'code: '    + error.code    + '\n' +
+					'message: ' + error.message + '\n');
+				alert('Where in the fuck are you?! (An error occured while determining your location)');
 			}
 
 			navigator.geolocation.getCurrentPosition(onSuccess, onError);
@@ -50,29 +52,51 @@ $(document).ready(function() {
 		var service;
 		var infoWindow;
 		
-		map = new google.maps.Map(document.getElementById('map'), {
+		map = new google.maps.Map(document.getElementById('map-canvas'), {
 			center: position,
 			zoom: 15
 		});
 
 		var request = {
 			location: position,
-			radius: '500',
+			radius: '250',
 			openNow: true,
 			types: ['convenience_store']
 		};
 
 		infowindow = new google.maps.InfoWindow();
 		service = new google.maps.places.PlacesService(map);
-		service.nearbySearch(request, callback);
+		service.nearbySearch(request, searchCallback);
 
-		function callback(results, status) {
+		function searchCallback(results, status) {
 			if (status == google.maps.places.PlacesServiceStatus.OK) {
+				console.log("Found " + results.length + " places with smokes");
+				
+				//create a marker for each location and expand the map to show all points
+				var bounds = new google.maps.LatLngBounds(position, position);
 				$.each(results, function(index, place) {
 					createMarker(place);
+					bounds.extend(place.geometry.location);
 				});
+				map.fitBounds(bounds);
+				
+				$("#map-canvas").hide().removeClass('hide').fadeIn();
+			}
+			else if(status == google.maps.places.PlacesServiceStatus.ZERO_RESULTS) {
+				//expand search radius and try again
+				var newRadius = parseInt(request.radius) * 2;
+				
+				if(newRadius > 32000) {
+					alert("This is fucked! There's nowhere to get smokes anywhere near you!");
+					return;
+				}
+				
+				console.log("Expanding search radius to " + newRadius + " meters");
+				request.radius = newRadius.toString();
+				service.nearbySearch(request, searchCallback);
 			}
 			else {
+				console.log("Error querying the the Google Map API:\n" + status);
 				alert("Sorry, something fucked up! Try again later.");
 			}
 		}
